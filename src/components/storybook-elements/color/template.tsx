a5066d49d523bee8db5efdd55ac23b019ae4aa55
DAN001\SD073437 <aliye.kurumlu-vankerckhove@sdworx.com>
2022-06-09 15:15:12 +0200

    updating the brand color as it is sdworx design portal defined

--- src/components/storybook-elements/color/template.tsx
+++ src/components/storybook-elements/color/template.tsx
@@ -2,50 +2,46 @@ import { FunctionComponent } from 'react';
 import * as React from 'react';
 
 export const BrandColorPrimary: FunctionComponent = () => (
-  <div className="storybook-style-color-tile">
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--color-primary"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-primary-light"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-primary-dark"></div>
+  <div className="storybook-style-color-tile storybook-style-color-brand-tile">
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--color-primary"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-primary-light-60"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-primary-light-40"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-primary-light-20"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-primary-dark-140"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-primary-dark-120"></div>
   </div>
 );
 
 export const BrandColorSecondary: FunctionComponent = () => (
-  <div className="storybook-style-color-tile">
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--color-secondary"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-secondary-light"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-secondary-dark"></div>
+  <div className="storybook-style-color-tile storybook-style-color-brand-tile">
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--color-secondary"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-secondary-light-60"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-secondary-light-40"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-secondary-light-20"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-secondary-dark-140"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-secondary-dark-120"></div>
   </div>
 );
 
 export const BrandColorTertiary: FunctionComponent = () => (
-  <div className="storybook-style-color-tile">
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--color-tertiary"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-tertiary-light"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-tertiary-dark"></div>
+  <div className="storybook-style-color-tile storybook-style-color-brand-tile">
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--color-tertiary"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-tertiary-light-60"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-tertiary-light-40"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-tertiary-light-20"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-tertiary-dark-140"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-tertiary-dark-120"></div>
   </div>
 );
 
 export const BrandColorQuaternary: FunctionComponent = () => (
-  <div className="storybook-style-color-tile">
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--color-quaternary"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quaternary-light"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quaternary-dark"></div>
-  </div>
-);
-
-export const BrandColorQuinary: FunctionComponent = () => (
-  <div className="storybook-style-color-tile">
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--color-quinary"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quinary-light"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quinary-dark"></div>
-  </div>
-);
-
-export const BrandColorSenary: FunctionComponent = () => (
-  <div className="storybook-style-color-tile">
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--color-senary"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-senary-light"></div>
-    <div className="storybook-style-color-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-senary-dark"></div>
+  <div className="storybook-style-color-tile storybook-style-color-brand-tile">
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--color-quaternary"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quaternary-light-60"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quaternary-light-40"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quaternary-light-20"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quaternary-dark-140"></div>
+    <div className="storybook-style-color-tile__item storybook-style-color-brand-tile__item storybook-style-color-tile__item--small storybook-style-color-tile__item--color-quaternary-dark-120"></div>
   </div>
 );
 
